88fbd312b4adf2d407ddba4588cceb2b31415b8e
Ingvar Derešivski <inde999@gmail.com>
2025-02-26 11:25:26 +0200

    doubleInt

---
+++ 02. 26.02.2025/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data types!");
+
+            //täisnumber
+            int bigNumber = 1234567890;
+            Console.WriteLine(bigNumber);
+
+            //komaga number ??
+            double comaNumber = 12.5;
+            Console.WriteLine(comaNumber);
+        }
+    }
+}
