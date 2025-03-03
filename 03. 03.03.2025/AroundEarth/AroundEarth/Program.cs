e8bb77afb8e4c514477b3b909f79ab046dbd39e0
Ingvar Derešivski <inde999@gmail.com>
2025-03-03 15:06:49 +0200

    AroundEarth

---
+++ 03. 03.03.2025/AroundEarth/AroundEarth/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AroundEarth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //vaja arvutada, mitu kahe eurost münti läheb ümber maa
+            //maa ümbermõõt tuleb välja arvutada raadiuse abil
+
+            double coinDiameter = 25.75; //mm
+            double earthRadius = 6378000000;
+            double aroundEarth = 2 * Math.PI * earthRadius;
+
+            Console.WriteLine("Maa ümbermõõt: " + aroundEarth);
+
+            //nüüd alles müntide arv välja arvutada
+            double howManyCoins = aroundEarth / coinDiameter;
+
+            Console.WriteLine("Ümber maa mahub nii palju euroseid " + howManyCoins);
+        }
+    }
+}
