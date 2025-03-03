56ea1b288e95b25b53899a581c290c67124dacc9
Ingvar Derešivski <inde999@gmail.com>
2025-03-03 14:08:54 +0200

    BracketsIfElseString

---
+++ 03. 03.03.2025/Brackets/Brackets/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Kui palju on 3 + 8 / (4 - 2) * 4 = ?");
+
+            //miks arvuti selliselt arvutab?
+            //
+            double calculation1 = 3 + 8 / (4 - 2) * 4;
+            //
+            double calculation2 = (3 + 8) / (4 - 2) * 4;
+            double calculation3 = (3 + 8) / (4 - 2) * 4;
+            double calculation4 = ((3.0 + 8.0) / (4.0 - 2.0) * 4.0);
+
+            Console.WriteLine("Vastus: " + calculation1);
+            Console.WriteLine("Vastus: " + calculation2);
+            Console.WriteLine("Vastus: " + calculation3);
+            Console.WriteLine("Vastus: " + calculation4);
+        }
+    }
+}
---
+++ 03. 03.03.2025/StringIfAndElse/StringIfAndElse/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace StringIfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //peab saama sisestada nime
+            //if ja else kontrollib, kas on nimi või on tühi
+            //kui leiate kuskilt teksti värviliseks teha, siis kasutage
+
+            string name = Console.ReadLine();
+
+            if (name != "")
+            {
+                Console.WriteLine("Sisestasid nime: " + name);
+            }
+            else
+            {
+                Console.WriteLine("Ei sisestanud nime");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR");
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+            }
+        }
+    }
+}
