7cbbc1c5dbb33d46d25701e1e693eab8125f891e
Ingvar Derešivski <inde999@gmail.com>
2025-03-03 15:17:44 +0200

    uhuu

---
+++ 03. 03.03.2025/ForLoop/ForLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int uhuu = 3;
+
+            for (int i = 0; i < uhuu; i++)
+            {
+                Console.WriteLine("Tere uhuu");
+            }
+        }
+    }
+}
